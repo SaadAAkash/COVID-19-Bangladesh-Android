e7d19c080df86c50fed17eacefe78bbeb133c599
-1 <sa.akash0129@gmail.com>
2020-03-19 05:25:32 +0600

    base response added

---
+++ app/src/main/java/ninja/saad/palaocorona/data/base/BaseResponse.kt
@@ -0,0 +1,15 @@
+package ninja.saad.palaocorona.data.base
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+open class BaseResponse : Serializable {
+
+    @SerializedName("data")
+    val data: String? = ""
+    @SerializedName("status")
+    val status: Int? = 0
+    @SerializedName("message")
+    val message: Boolean? = false
+
+}
