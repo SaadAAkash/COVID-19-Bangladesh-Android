272c9351469befe1d1766f46090a5d621d26adf9
Shafayat <shafayathossainkhan@gmail.cm>
2020-03-19 21:25:41 +0600

    dashboard fragment UI done
    6 buttons added in DashboardFragment. Also language changing button added in actionbar and language change functionality done.

--- app/src/main/java/ninja/saad/palaocorona/base/BaseApplication.kt
+++ app/src/main/java/ninja/saad/palaocorona/base/BaseApplication.kt
@@ -1,22 +1,34 @@
 package ninja.saad.palaocorona.base
 
+import android.content.Context
+import android.content.res.Configuration
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.ProcessLifecycleOwner
+import com.franmontiel.localechanger.LocaleChanger
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 import ninja.saad.palaocorona.di.components.DaggerAppComponent
+import java.util.*
 import javax.inject.Inject
 
 class BaseApplication : DaggerApplication(), LifecycleObserver {
-
+    
     override fun onCreate() {
         super.onCreate()
-
         ProcessLifecycleOwner.get()
             .lifecycle
             .addObserver(this)
+        LocaleChanger.initialize(applicationContext, mutableListOf(Locale("en"), Locale("bn")));
+        Logger.addLogAdapter(AndroidLogAdapter())
     }
-
+    
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        LocaleChanger.onConfigurationChanged()
+    }
+    
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder()
             .application(this)
--- app/src/main/java/ninja/saad/palaocorona/base/ui/FragmentCommunicator.kt
+++ app/src/main/java/ninja/saad/palaocorona/base/ui/FragmentCommunicator.kt
@@ -1,8 +1,10 @@
 package ninja.saad.palaocorona.base.ui
 
 import android.os.Bundle
+import java.util.*
 
 interface FragmentCommunicator {
 
     fun startActivity(clz: Class<*>?, bundle: Bundle?)
+    fun toggleLanguage()
 }
