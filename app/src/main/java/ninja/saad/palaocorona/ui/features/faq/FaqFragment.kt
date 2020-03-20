8ee1263b4f953b2370dbde9ac69054ef61591a17
-1 <sa.akash0129@gmail.com>
2020-03-20 09:16:43 +0600

    faq new ui & animation manager added

--- app/src/main/java/ninja/saad/palaocorona/ui/features/faq/FaqFragment.kt
+++ app/src/main/java/ninja/saad/palaocorona/ui/features/faq/FaqFragment.kt
@@ -8,6 +8,7 @@ import kotlinx.android.synthetic.main.fragment_faq.*
 import kotlinx.android.synthetic.main.item_faq_2.*
 import ninja.saad.palaocorona.R
 import ninja.saad.palaocorona.base.ui.BaseFragment
+import ninja.saad.palaocorona.util.AnimationManager
 import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class FaqFragment: BaseFragment<FaqViewModel>() {
@@ -17,7 +18,6 @@ class FaqFragment: BaseFragment<FaqViewModel>() {
     
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initViews()
         
         rvFaq.layoutManager = LinearLayoutManager(context)
         rvFaq.adapter = FaqAdapter()
@@ -27,37 +27,5 @@ class FaqFragment: BaseFragment<FaqViewModel>() {
         })
         
         viewModel.getFaq()
-    }
-    
-    private fun initViews() {
-        btn_expand.onClick {
-            //toggleSectionInfo(btn_expand);
-        }
-        btn_hide.onClick {
-            //toggleSectionInfo(btn_hide);
-        }
-    }
-    
-    /*private fun toggleSectionInfo(view: View) {
-        val show: Boolean = toggleArrow(view)
-        if (show) {
-            ViewAnimation.expand(layout_faq_answer, object : AnimListener() {
-                fun onFinish() {
-                    Tools.nestedScrollTo(nested_scroll_view, layout_faq_answer)
-                }
-            })
-        } else {
-            ViewAnimation.collapse(layout_faq_answer)
-        }
-    }*/
-    
-    fun toggleArrow(view: View): Boolean {
-        return if (view.rotation == 0f) {
-            view.animate().setDuration(200).rotation(180f)
-            true
-        } else {
-            view.animate().setDuration(200).rotation(0f)
-            false
-        }
     }
 }
