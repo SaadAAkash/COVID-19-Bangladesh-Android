33ccf57d34a936356c960a57780480664c03d07a
-1 <sa.akash0129@gmail.com>
2020-03-21 07:01:59 +0600

    Slider images for both Quarantine & Dos n Donts implemented

--- app/src/main/java/ninja/saad/palaocorona/ui/features/quarantine/QuarantineFragment.kt
+++ app/src/main/java/ninja/saad/palaocorona/ui/features/quarantine/QuarantineFragment.kt
@@ -1,9 +1,16 @@
 package ninja.saad.palaocorona.ui.features.quarantine
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
+import com.smarteist.autoimageslider.IndicatorAnimations
+import com.smarteist.autoimageslider.SliderAnimations
+import com.smarteist.autoimageslider.SliderView
+import kotlinx.android.synthetic.main.fragment_slider_images.*
 import ninja.saad.palaocorona.R
 import ninja.saad.palaocorona.base.ui.BaseFragment
+import ninja.saad.palaocorona.ui.features.dashboard.SliderAdapter
 
 class QuarantineFragment: BaseFragment<QuarantineViewModel>()  {
     
@@ -12,5 +19,13 @@ class QuarantineFragment: BaseFragment<QuarantineViewModel>()  {
     
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        image_slider.sliderAdapter = SliderAdapter()
+        //drop or swap
+        image_slider.setIndicatorAnimation(IndicatorAnimations.DROP); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        image_slider.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        image_slider.scrollTimeInSec = 4; //set scroll delay in seconds :
+        //image_slider.startAutoCycle();
+        image_slider.autoCycleDirection = SliderView.AUTO_CYCLE_DIRECTION_LEFT;
+    
     }
 }
