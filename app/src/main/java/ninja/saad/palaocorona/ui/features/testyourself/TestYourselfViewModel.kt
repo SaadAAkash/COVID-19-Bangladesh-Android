0cc50e0b240c3cce21525dc7ff59fa4e4d907980
Shafayat <shafayathossainkhan@gmail.cm>
2020-03-21 13:53:17 +0600

    normal and chip radio view added dynamically

--- app/src/main/java/ninja/saad/palaocorona/ui/features/testyourself/TestYourselfViewModel.kt
+++ app/src/main/java/ninja/saad/palaocorona/ui/features/testyourself/TestYourselfViewModel.kt
@@ -5,22 +5,46 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import ninja.saad.palaocorona.base.ui.BaseViewModel
 import ninja.saad.palaocorona.data.testyourself.TestYourselfRepository
-import ninja.saad.palaocorona.data.testyourself.model.Questionnaire
+import ninja.saad.palaocorona.data.testyourself.model.Question
+import ninja.saad.palaocorona.util.SingleLiveEvent
 import javax.inject.Inject
 
 class TestYourselfViewModel @Inject constructor(private val repository: TestYourselfRepository): BaseViewModel() {
     
-    var questionnaire = MutableLiveData<MutableList<Questionnaire>>()
+    var questionnaire = SingleLiveEvent<MutableList<Question>>()
+    var currentIndex = 0
+    private var allQuestionnaire = mutableListOf<Question>()
     
     fun getQuestionnaire() {
-        val disposable = repository.getQuestionnaire()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
-                this.questionnaire.value = it
-            }, {
-                it.printStackTrace()
-            })
+        if(allQuestionnaire.size == 0) {
+            val disposable = repository.getQuestionnaire()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    this.allQuestionnaire = it
+                    if((currentIndex + 2) < allQuestionnaire.size) {
+                        this.questionnaire.value =
+                            allQuestionnaire.subList(currentIndex, currentIndex + 2)
+                        currentIndex += 2
+                    }
+                }, {
+                    it.printStackTrace()
+                })
+            compositeDisposable.add(disposable)
+        } else {
+            if((currentIndex + 2) < allQuestionnaire.size) {
+                this.questionnaire.value = allQuestionnaire.subList(currentIndex, currentIndex + 2)
+                currentIndex += 2
+            }
+        }
         
+    }
+    
+    fun setAnswer(question: Question, answer: String) {
+        allQuestionnaire[allQuestionnaire.indexOf(question)] = question.apply {
+            val position = texts.indexOf(answer)
+            selectedAnswer = texts[position]
+            selectedAnswerPosition = position
+        }
     }
 }
---
+++ app/src/main/java/ninja/saad/palaocorona/util/TYPE.kt
@@ -0,0 +1,7 @@
+package ninja.saad.palaocorona.util
+
+enum class TYPE(val value: String) {
+	NORMAL("normal"),
+	CHIP("chip"),
+	BUTTON("button")
+}
