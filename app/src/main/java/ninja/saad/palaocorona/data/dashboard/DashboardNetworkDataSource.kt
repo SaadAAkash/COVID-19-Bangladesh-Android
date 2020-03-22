e9604c878b2e9e4fa6c86a3e5fe0b60f86a355f6
Shafayat <shafayathossainkhan@gmail.cm>
2020-03-22 09:57:00 +0600

    gujob naki slider images fetched from firebase storage

--- app/src/main/java/ninja/saad/palaocorona/data/dashboard/DashboardNetworkDataSource.kt
+++ app/src/main/java/ninja/saad/palaocorona/data/dashboard/DashboardNetworkDataSource.kt
@@ -1,11 +1,47 @@
 package ninja.saad.palaocorona.data.dashboard
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.storage.FirebaseStorage
+import io.reactivex.Single
 import javax.inject.Inject
 
 class DashboardNetworkDataSource @Inject constructor() {
     
+    companion object {
+        const val DIR_SLIDER_IMAGES = "gujob_na_ki"
+    }
+    
     fun isUserLoggedIn(): Boolean {
         return FirebaseAuth.getInstance().uid != null
+    }
+    
+    fun getSliderImages(): Single<MutableList<String>> {
+        return Single.create { emitter ->
+            val urls = mutableListOf<String>()
+            val storage = FirebaseStorage.getInstance()
+            storage.reference
+                .child(DIR_SLIDER_IMAGES)
+                .listAll()
+                .addOnSuccessListener { result ->
+                    result.items.forEach { reference ->
+                        reference.downloadUrl.addOnSuccessListener {
+                            urls.add(it.toString())
+                            if(urls.size == result.items.size) {
+                                emitter.onSuccess(urls)
+                            }
+                        }.addOnFailureListener {
+                            if(!emitter.isDisposed){
+                                emitter.onError(it)
+                            }
+                            it.printStackTrace()
+                        }
+                    }
+                }.addOnFailureListener {
+                    if(!emitter.isDisposed){
+                        emitter.onError(it)
+                    }
+                }
+        }
     }
 }
--- app/src/main/java/ninja/saad/palaocorona/ui/features/dashboard/DashboardViewModel.kt
+++ app/src/main/java/ninja/saad/palaocorona/ui/features/dashboard/DashboardViewModel.kt
@@ -1,12 +1,31 @@
 package ninja.saad.palaocorona.ui.features.dashboard
 
+import androidx.lifecycle.MutableLiveData
+import com.orhanobut.logger.Logger
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import ninja.saad.palaocorona.base.ui.BaseViewModel
 import ninja.saad.palaocorona.data.dashboard.DashboardRepository
 import javax.inject.Inject
 
 class DashboardViewModel @Inject constructor(val repository: DashboardRepository): BaseViewModel() {
     
+    var sliderImages = MutableLiveData<MutableList<String>>()
+    
     fun isUserLoggedIn(): Boolean {
         return repository.isUserLoggedIn()
+    }
+    
+    fun getSliderImages() {
+        val disposable = repository.getSliderImages()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Logger.d(it)
+                sliderImages.value = it
+            }, {
+                it.printStackTrace()
+            })
+        compositeDisposable.add(disposable)
     }
 }
