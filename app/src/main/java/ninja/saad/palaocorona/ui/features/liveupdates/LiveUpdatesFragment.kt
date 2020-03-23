2db6a5f3f8c51c05fb85078f16cc150a374c638a
-1 <sa.akash0129@gmail.com>
2020-03-23 08:44:47 +0600

    Fragment Live Updates added

--- app/src/main/java/ninja/saad/palaocorona/ui/features/liveupdates/LiveUpdatesFragment.kt
+++ app/src/main/java/ninja/saad/palaocorona/ui/features/liveupdates/LiveUpdatesFragment.kt
@@ -1,4 +1,17 @@
 package ninja.saad.palaocorona.ui.features.liveupdates
 
-class LiveUpdatesFragment {
+import android.os.Bundle
+import android.view.View
+import ninja.saad.palaocorona.R
+import ninja.saad.palaocorona.base.ui.BaseFragment
+import ninja.saad.palaocorona.ui.features.about.AboutCovidViewModel
+
+class LiveUpdatesFragment : BaseFragment<AboutCovidViewModel>()  {
+    
+    override val layoutId: Int
+    get() = R.layout.fragment_live_updates
+    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
