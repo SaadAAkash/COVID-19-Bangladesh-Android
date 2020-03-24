33ff1f7f67df9507ae297c52c001d0b8b0ad1e70
Shafayat <shafayathossainkhan@gmail.cm>
2020-03-24 09:34:06 +0600

    news pagination added

--- app/src/main/java/ninja/saad/palaocorona/ui/features/news/NewsFragment.kt
+++ app/src/main/java/ninja/saad/palaocorona/ui/features/news/NewsFragment.kt
@@ -1,5 +1,7 @@
 package ninja.saad.palaocorona.ui.features.news
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.Observer
@@ -8,7 +10,7 @@ import kotlinx.android.synthetic.main.fragment_news.*
 import ninja.saad.palaocorona.R
 import ninja.saad.palaocorona.base.ui.BaseFragment
 
-class NewsFragment: BaseFragment<NewsViewModel>()  {
+class NewsFragment: BaseFragment<NewsViewModel>(), NewsAdapter.NewsAdapterCallback {
     
     override val layoutId: Int
         get() = R.layout.fragment_news
@@ -17,12 +19,27 @@ class NewsFragment: BaseFragment<NewsViewModel>()  {
         super.onViewCreated(view, savedInstanceState)
         
         rvNews.layoutManager = LinearLayoutManager(context)
-        rvNews.adapter = NewsAdapter()
+        rvNews.adapter = NewsAdapter(this)
         
         viewModel.news.observe(viewLifecycleOwner, Observer {
             (rvNews.adapter as NewsAdapter).setItems(it)
         })
         
         viewModel.getNews(getCurrentLocale().language, 0)
+    }
+    
+    override fun onNewsClick(url: String) {
+        try {
+            val webIntent = Intent(Intent.ACTION_VIEW)
+            val Url = url
+            webIntent.data = Uri.parse(Url)
+            startActivity(webIntent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+    
+    override fun loadMore(size: Int) {
+        viewModel.getNews(getCurrentLocale().language, size)
     }
 }
