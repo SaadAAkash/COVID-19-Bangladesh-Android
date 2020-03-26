bafe3a54144b5041415902d9765c1581893e7507
Shafayat <shafayathossainkhan@gmail.cm>
2020-03-26 16:31:28 +0600

    is logged in check changed

--- app/src/main/java/xyz/palaocorona/data/main/MainRepositoryImpl.kt
+++ app/src/main/java/xyz/palaocorona/data/main/MainRepositoryImpl.kt
@@ -2,14 +2,16 @@ package xyz.palaocorona.data.main
 
 import io.reactivex.Single
 import xyz.palaocorona.BuildConfig
+import xyz.palaocorona.base.data.local.AppPreference
 import xyz.palaocorona.data.authentication.AuthenticationDataSource
 import javax.inject.Inject
 
-class MainRepositoryImpl @Inject constructor(private val authenticationDataSource: AuthenticationDataSource,
+class MainRepositoryImpl @Inject constructor(private val preference: AppPreference,
+                                             private val authenticationDataSource: AuthenticationDataSource,
                                              private val mainNetworkDataSource: MainNetworkDataSource ): MainRepository {
     
     override fun isLoggedIn(): Boolean {
-        return authenticationDataSource.isUserLoggedIn()
+        return authenticationDataSource.isUserLoggedIn() && preference.user.name.isNotEmpty()
     }
     
     override fun checkForUpdate(): Single<Boolean> {
