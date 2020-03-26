47baed5b390c10ccf4fcf0501c98b635dcf8ce7d
-1 <sa.akash0129@gmail.com>
2020-03-26 06:26:23 +0600

    COVID 19 fragment content added

--- app/src/main/java/ninja/saad/palaocorona/ui/features/about/AboutCovidFragment.kt
+++ app/src/main/java/ninja/saad/palaocorona/ui/features/about/AboutCovidFragment.kt
@@ -1,7 +1,10 @@
 package ninja.saad.palaocorona.ui.features.about
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.View
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_about_covid.*
 import ninja.saad.palaocorona.R
 import ninja.saad.palaocorona.base.ui.BaseFragment
 
@@ -12,5 +15,18 @@ class AboutCovidFragment: BaseFragment<AboutCovidViewModel>()  {
     
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        tv_about_covid_source.movementMethod = LinkMovementMethod.getInstance()
+        context?.let {
+            if (getCurrentLocale().language == "bn")
+                Glide.with(it)
+                    .asGif()
+                    .load("https://palaocorona.xyz/gif/Gif_Bangla.gif")
+                    .into(ivAboutCovid)
+            if (getCurrentLocale().language == "en")
+                Glide.with(it)
+                    .asGif()
+                    .load("https://palaocorona.xyz/gif/Gif_English.gif")
+                    .into(ivAboutCovid)
+        }
     }
 }
