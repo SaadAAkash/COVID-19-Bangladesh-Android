341d591a0d8676557caa1de4c654f537e47411f5
Shafayat <shafayathossainkhan@gmail.cm>
2020-03-26 22:57:02 +0600

    android 7 crash fixed, splash theme removed

--- app/src/main/java/xyz/palaocorona/ui/features/main/MainActivity.kt
+++ app/src/main/java/xyz/palaocorona/ui/features/main/MainActivity.kt
@@ -34,7 +34,6 @@ class MainActivity : BaseActivity<MainViewModel>() {
     private var alertDialog: AlertDialog? = null
     
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         
         val spannable = SpannableString(getString(R.string.app_name_top_bar))
