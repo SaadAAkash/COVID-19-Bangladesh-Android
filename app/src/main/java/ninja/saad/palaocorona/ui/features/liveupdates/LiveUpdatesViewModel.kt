72e5c94bb18697236fcb05ccc0cd831579b1a2b9
-1 <sa.akash0129@gmail.com>
2020-03-26 08:13:59 +0600

    Live updates more data sources added with new UI

--- app/src/main/java/ninja/saad/palaocorona/ui/features/liveupdates/LiveUpdatesViewModel.kt
+++ app/src/main/java/ninja/saad/palaocorona/ui/features/liveupdates/LiveUpdatesViewModel.kt
@@ -6,12 +6,14 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import ninja.saad.palaocorona.base.ui.BaseViewModel
 import ninja.saad.palaocorona.data.liveupdates.LiveUpdateRepository
+import ninja.saad.palaocorona.data.liveupdates.model.LiveUpdateDataModelDGHS
 import ninja.saad.palaocorona.data.liveupdates.model.LiveUpdateResponse
 import javax.inject.Inject
 
 class LiveUpdatesViewModel @Inject constructor(val repository: LiveUpdateRepository): BaseViewModel() {
     
     var liveUpdates = MutableLiveData<LiveUpdateResponse>()
+    var liveUpdatesDGHS = MutableLiveData<LiveUpdateDataModelDGHS>()
     
     fun getLiveUpdates() {
         if(liveUpdates.value == null) {
@@ -21,6 +23,21 @@ class LiveUpdatesViewModel @Inject constructor(val repository: LiveUpdateReposit
                 .subscribe({
                     Logger.d(it)
                     liveUpdates.value = it
+                }, {
+                    it.printStackTrace()
+                })
+            compositeDisposable.add(disposable)
+        }
+    }
+    
+    fun getLiveUpdatesDGHS() {
+        if(liveUpdates.value == null) {
+            val disposable = repository.getLiveUpdateDGHS()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    Logger.d(it)
+                    liveUpdatesDGHS.value = it
                 }, {
                     it.printStackTrace()
                 })
